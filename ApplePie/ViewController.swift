a5532b9375fda65766483f174e40c566a22dd6f1
Igor Guryan <sacujo@mail.ru>
2019-10-03 12:23:49 +0300

    Initial Commit

---
+++ ApplePie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ApplePie
+//
+//  Created by Igor Guryan on 03/10/2019.
+//  Copyright © 2019 Igor Guryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
